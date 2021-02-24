99663c54e648f641656ad6f0fa50b8e991bf6d37
Sean Rodriguez <sean.rodriguez@outlook.com>
2021-02-24 13:39:45 -0500

    Remove Entity Framework Core

--- Source/Persistence/DependencyInjection.cs
+++ Source/Persistence/DependencyInjection.cs
@@ -1,6 +1,4 @@
-﻿using Application.Common.Interfaces;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
+﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Persistence
@@ -9,11 +7,6 @@ namespace Persistence
     {
         public static IServiceCollection AddPersistence(this IServiceCollection services, IConfiguration configuration)
         {
-            services.AddDbContext<JobContext>(options => 
-                options.UseMySQL(configuration.GetConnectionString("JobHuntDB")));
-
-            services.AddScoped<IJobContext>(provider => provider.GetService<JobContext>());
-
             return services;
         }
     }
