7e0b2b209470719184a0e32d7a90b0a097cecd09
Sean Rodriguez <sean.rodriguez@outlook.com>
2021-02-24 21:16:13 -0500

    Create JobRepository class

---
+++ Source/Persistence/JobRepository.cs
@@ -0,0 +1,55 @@
+﻿using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.Model;
+using Application.Common.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Persistence
+{
+    public class JobRepository : IRepository<Job>
+    {
+        private readonly AmazonDynamoDBClient _client;
+
+        public JobRepository(AmazonDynamoDBClient client) 
+        {
+            _client = client;
+        }
+
+        public void Add(Job entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddRange(IEnumerable<Job> entities)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Job> Find(Expression<Func<Job, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Job Get(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Job> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(Job entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveRange(IEnumerable<Job> entities)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
