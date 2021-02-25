725a756efe9d4afcad8b18068f20952d5c16efb6
Sean Rodriguez <sean.rodriguez@outlook.com>
2021-02-24 21:16:42 -0500

    Create IRepository interface

---
+++ Source/Application/Common/Interfaces/IRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace Application.Common.Interfaces
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        TEntity Get(Guid id);
+        IEnumerable<TEntity> GetAll();
+        IEnumerable<TEntity> Find(Expression<Func<TEntity, bool>> predicate);
+
+
+        void Add(TEntity entity);
+        void AddRange(IEnumerable<TEntity> entities);
+
+        void Remove(TEntity entity);
+        void RemoveRange(IEnumerable<TEntity> entities);
+    }
+}
