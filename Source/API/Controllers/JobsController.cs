45bf4bd908aceda80c2117314819f0e755840419
Sean Rodriguez <sean.rodriguez@outlook.com>
2021-03-02 12:22:36 -0500

    Rename JobController to JobsController; Add Get, GetAll and Add endpoints

---
+++ Source/API/Controllers/JobsController.cs
@@ -0,0 +1,40 @@
+﻿using Application.Common.Interfaces;
+using Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class JobsController : ControllerBase
+    {
+        private readonly IJobRepository _jobs;
+
+        public JobsController(IJobRepository jobs)
+        {
+            _jobs = jobs;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var result = await _jobs.GetAllAsync();
+            return Ok(result);
+        }
+
+        [HttpGet("{JobId}")]
+        public async Task<IActionResult> Get(string jobId)
+        {
+            Job job = await _jobs.GetAsync(jobId);
+            return Ok(job);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add([FromBody] Job job)
+        {
+            await _jobs.AddAsync(job);
+            return Created("", job);
+        }
+    }
+}
