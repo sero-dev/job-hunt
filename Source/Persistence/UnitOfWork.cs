469f285c39793ba2e37baad1012a1682b0419d1d
Sean Rodriguez <sean.rodriguez@outlook.com>
2021-03-04 18:02:33 -0500

    Create Unit Of Work classes

---
+++ Source/Persistence/UnitOfWork.cs
@@ -0,0 +1,19 @@
+﻿using Application.Common.Interfaces;
+using Persistence.Repository;
+
+namespace Persistence
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly JobContext _context;
+
+        public IJobRepository Jobs { get; private set; }
+
+        public UnitOfWork(JobContext context)
+        {
+            _context = context;
+            Jobs = new JobRepository(_context);
+        }
+
+    }
+}
