64f79b4bd3fb554003a7e86714d93a8a0ed19f21
Sean Rodriguez <sean.rodriguez@outlook.com>
2021-03-07 21:54:36 -0500

    Exclude from Code Coverage report

--- Source/Application/DependencyInjection.cs
+++ Source/Application/DependencyInjection.cs
@@ -1,9 +1,11 @@
-﻿using MediatR;
+﻿using System.Diagnostics.CodeAnalysis;
+using MediatR;
 using Microsoft.Extensions.DependencyInjection;
 using System.Reflection;
 
 namespace Application
 {
+    [ExcludeFromCodeCoverage]
     public static class DependencyInjection
     {
         public static IServiceCollection AddApplication(this IServiceCollection services)
