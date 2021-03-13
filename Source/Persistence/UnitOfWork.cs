15b8cc43d31bb3aa4c8f7d9f844b3f0578cfb017
Sean Rodriguez <sean.rodriguez@outlook.com>
2021-03-13 12:46:09 -0500

    Remove AWS SDK libraries from using namespaces

--- Source/Persistence/UnitOfWork.cs
+++ Source/Persistence/UnitOfWork.cs
@@ -1,6 +1,4 @@
-﻿using Amazon.DynamoDBv2;
-using Amazon.DynamoDBv2.DataModel;
-using Application.Common.Interfaces;
+﻿using Application.Common.Interfaces;
 using Microsoft.Extensions.Configuration;
 using MongoDB.Driver;
 using Persistence.Repositories;
